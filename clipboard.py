fc500e2d6210c4e232be8c0efb4ed065c70f54e3
woshichuanqilz <513278236@qq.com>
2016-01-07 21:17:38 +0800

    add 3 file

---
+++ clipboard.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+#This's a sample of operate the clipboard
+ 
+import win32clipboard as wincb
+import win32con
+ 
+# wincb.OpenClipboard()
+# wincb.EmptyClipboard()
+# wincb.SetClipboardData(win32con.CF_TEXT, "Hello World!")  #复制文本内容到剪贴板，系统后台会返回内存地址
+print wincb.GetClipboardData(win32con.CF_TEXT).encoding()  #'Hello World!'
+wincb.CloseClipboard()
---
+++ encoding.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+print "请将文件拖曳到本脚本！".decode('utf-8').encode('gbk')
+print "请将文件拖曳到本脚本！"
