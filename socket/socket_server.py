40b31396321b3ad792a3b26555e3243161c8e349
zhe li <zhe li>
2016-07-04 16:44:02 +0800

    modify docket

--- socket/socket_server.py
+++ socket/socket_server.py
@@ -8,12 +8,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # s = socket.socket()
 s.bind(address)
 s.listen(5)
 
-ss, addr = s.accept()
-print('got connected from',addr)
+while True:
+    ss, addr = s.accept()
+    print('got connected from',addr)
 
-ss.send('byebye')
-ra = ss.recv(512)
-print(ra)
+    # ss.send('byebye')
+    ra = ss.recv(512)
+    print(ra)
+    ss.close()
 
-ss.close()
 s.close()
