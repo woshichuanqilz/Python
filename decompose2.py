8d1d2a15f1de1f9abf9ee7a207d8652ebd0d0cd0
zhe li <zhe li>
2016-07-04 09:05:22 +0800

    pytts

--- decompose2.py
+++ decompose2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 #coding:utf-8
 import sys   ##加载sys这个模块。
-decomposeNum = (int)(sys.argv[1])
-for num in range(0, 10):
-    if decomposeNum & 2 ** num != 0:
-        print 2 ** num
+for i in range(len(sys.argv)):
+    print "第%d个参数是：%s" % (i,sys.argv[i])
--- encoding.py
+++ encoding.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
 print "请将文件拖曳到本脚本！".decode('utf-8').encode('gbk')
 print "请将文件拖曳到本脚本！"
+print "李哲"
+    
