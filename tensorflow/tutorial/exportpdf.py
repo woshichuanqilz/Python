75cbe42bf7d095bcea03b978392219ac154a4243
woshichuanqilz <513278236@qq.com>
2016-07-31 15:34:15 +0800

    tensorflow

---
+++ tensorflow/tutorial/exportpdf.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from sklearn.externals.six import StringIO  
+import pydot 
+dot_data = StringIO() 
+tree.export_graphviz(clf, out_file=dot_data) 
+graph = pydot.graph_from_dot_data(dot_data.getvalue()) 
+graph.write_pdf("iris.pdf")
