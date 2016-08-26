18506a958e666e45179343b5a29e711181ff9cdc
zhe li <zhe li>
2016-08-26 20:37:03 +0800

    update pic spider

--- scrapy/downloadimg/downloadimg/spiders/dlimg.py
+++ scrapy/downloadimg/downloadimg/spiders/dlimg.py
@@ -4,7 +4,8 @@ import MySQLdb
 
 class DownloadingImg(scrapy.Spider):
     name = 'dlimg'
-    start_urls = ['https://pixabay.com/zh/']
+    # start_urls = ['https://pixabay.com/zh/']
+    start_urls = ['https://www.pexels.com/']
 
     # DB info
     db = MySQLdb.connect(host="192.168.1.107",user="lizhe",passwd="", db="test") 
@@ -14,17 +15,19 @@ class DownloadingImg(scrapy.Spider):
         imagelist = response.css('img::attr(src)').extract()
         imagelistnew = [ x for x in imagelist if 'blank.gif' not in x ]
 
-        for imageurl in imagelistnew:
-            full_url = response.urljoin(imageurl)
+        print str(len(imagelistnew)) + 'record will be inserted'
+        for full_url in imagelistnew:
+            # full_url = response.urljoin(imageurl)
+            print full_url
             name_arr = full_url.split('/')[-1].split('-')
             del name_arr[-1]
             name = " ".join(name_arr)
             res = self.insert_data(name, full_url)    
 
     def insert_data(self, name, full_url):    
-        print 'name ' + name + 'full_url ' + full_url
+        # print 'name ' + name + 'full_url ' + full_url
         statement = "INSERT INTO imgstore (imgname, imgurl) VALUES ('{}', '{}');".format(name, full_url)
-        print statement
+        # print statement
 
         # self.cur.execute("SELECT * FROM employee")
         self.cur.execute(statement)
